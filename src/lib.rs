0218333946169ea3a0f1a93e7c270d4aefb17836
Swarnalatha Satyavolu <srs282@cornell.edu>
2025-05-10 18:42:54 -0400

    changed max value of number set

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 
 pub fn random_number_print() {
 
-    let random_number = rand::thread_rng().gen_range(1..=100);
+    let random_number = rand::thread_rng().gen_range(1..=10000);
 
     println!("Randomly Generated Number: {random_number}");
 }

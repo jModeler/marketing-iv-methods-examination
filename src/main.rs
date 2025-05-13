283c5d5d753362fe26f0f53fc79e5f06cfe59818
Swarnalatha Satyavolu <srs282@cornell.edu>
2025-05-13 13:34:44 -0400

    Added a module to generate a random vector of arbitrary dimension

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod utils;
 
-use utils::random_vector_gen::random_number_print;
+use utils::random_vector_gen::random_vector_generate;
 
 fn main() {
-    random_number_print();
+    let random_vector = random_vector_generate(10);
+    println!("Randomly generated 10x1 vector: {:?}", random_vector);
 }
--- src/utils/random_vector_gen.rs
+++ src/utils/random_vector_gen.rs
@@ -1,8 +1,10 @@
-use rand::Rng;
+use ndarray::{Array, Array2};
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Normal;
 
-pub fn random_number_print() {
+pub fn random_vector_generate(n:usize) -> Array2<f64>{
 
-    let random_number = rand::thread_rng().gen_range(1..=10000);
+    let random_vector = Array::random((n, 1), Normal::new(0., 9.).unwrap());
 
-    println!("Randomly Generated Number: {random_number}");
+    return random_vector;
 }

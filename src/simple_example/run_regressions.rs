7c9766e0095f43a9fc61262547802fbc4ecd6f58
Swarnalatha Satyavolu <srs282@cornell.edu>
2025-05-17 13:47:44 -0400

    Added a follow up line to extract the tuple items

--- src/simple_example/run_regressions.rs
+++ src/simple_example/run_regressions.rs
@@ -4,6 +4,8 @@ use ndarray::{Array2, Axis, concatenate};
 use linfa_linear::FittedLinearRegression;
 
 pub fn run_yx_regression(params: (usize, f64, f64, f64, f64, f64, f64, bool)) -> Result<FittedLinearRegression<f64>, String> {
+    let (n, beta, alpha_y, alpha_x, sigma_a, sigma_ex, sigma_ey, intercept) = params;
+    
     let ind_vars = match ind_var_generate(n, alpha_x, sigma_a, sigma_ex) {
         Ok(vars) => { vars }
         Err(err_msg) => {
